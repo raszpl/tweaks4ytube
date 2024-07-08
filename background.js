dbc7995afa8aef3513406d4f5ec49dfc0105cd55
Rasz_pl <citizenr@gmail.com>
2024-07-08 14:10:55 +0200

    Update background.js normal popup, no uninstall reporting

--- background.js
+++ background.js
@@ -238,7 +238,7 @@ chrome.windows.onFocusChanged.addListener(function (wId) {
 # MESSAGE LISTENER
 --------------------------------------------------------------*/
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	//console.log(message);
+	console.log(message);
 	//console.log(sender);
 
 	switch (message.action || message.name || message) {
@@ -266,40 +266,17 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 			});
 			break
 
-		case 'fixPopup':
-			//~ get the current focused tab and convert it to a URL-less popup (with same state and size)
-			chrome.windows.getLastFocused(w => {
-				chrome.tabs.query({
-					windowId: w.id,
-					active: true
-				}, ts => {
-					const tID = ts[0]?.id,
-						  data = { type: 'popup',
-								  state: w.state,
-								  width: parseInt(message.width, 10),
-								  height: parseInt(message.height, 10),
-								  left: 0,
-								  top: 20
-								 }
-
-					if (tID) {
-						data.tabId = tID;
-					}
-					chrome.windows.create(data);
-
-					//append to title?
-					chrome.tabs.onUpdated.addListener(function listener (tabId, changeInfo) {
-						if (tabId === tID && changeInfo.status === 'complete' && !message.title.startsWith("undefined")) {
-							chrome.tabs.onUpdated.removeListener(listener);
-							chrome.scripting.executeScript({ target: { tabId: tID }, func: () => {
-								document.title = `${message.title} - ImprovedTube`;
-							} });			//manifest3
-							// chrome.tabs.executeScript(tID, {code: `document.title = "${message.title} - ImprovedTube";`});  //manifest2
-						}
-					});
-				});
+		case 'popup':
+			chrome.windows.create({
+				type: 'popup',
+				width: message.width,
+				height: message.height,
+				left: 0,
+				top: 20,
+				url: message.url
 			});
 			break
+
 		case 'download':
 			chrome.permissions.request({
 				permissions: ['downloads'],
@@ -325,5 +302,3 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 			break
 	}
 });
-/*-----# UNINSTALL URL-----------------------------------*/
-chrome.runtime.setUninstallURL('https://improvedtube.com/uninstalled');
