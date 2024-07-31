acc2a15b4186a058f23b98143a2051e24b4f1ceb
Rasz_pl <citizenr@gmail.com>
2024-07-31 21:03:22 +0200

    linter

--- js&css/extension/init.js
+++ js&css/extension/init.js
@@ -44,27 +44,6 @@ chrome.runtime.sendMessage({
 	}
 });
 
-
-/*
-extension.inject([
-	'/js&css/web-accessible/core.js',
-	'/js&css/web-accessible/functions.js',
-	'/js&css/web-accessible/www.youtube.com/appearance.js',
-	'/js&css/web-accessible/www.youtube.com/themes.js',
-	'/js&css/web-accessible/www.youtube.com/player.js',
-	'/js&css/web-accessible/www.youtube.com/playlist.js',
-	'/js&css/web-accessible/www.youtube.com/channel.js',
-	'/js&css/web-accessible/www.youtube.com/shortcuts.js',
-	'/js&css/web-accessible/www.youtube.com/blocklist.js',
-	'/js&css/web-accessible/www.youtube.com/settings.js',
-	'/js&css/web-accessible/init.js'
-], function () {
-	extension.ready = true;
-
-	extension.events.trigger('init');
-});
-*/
-
 document.addEventListener('DOMContentLoaded', function () {
 	extension.domReady = true;
 
@@ -72,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	switch(message.action) {
+	switch (message.action) {
 		case 'focus':
 			extension.messages.send({
 				action: 'focus'
@@ -110,7 +89,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 				action: 'mixer'
 			}, sendResponse, 'mixer');
 			return true;
-			break
 
 		case 'delete-youtube-cookies':
 			extension.messages.send({
@@ -149,29 +127,29 @@ document.addEventListener('it-message-from-web-accessible', function (message) {
 			break
 
 		case 'analyzer':
-			if (extension.storage.data.analyzer_activation === true) {
-				var data = message.name,
+			if (extension.storage.data.analyzer_activation) {
+				const data = message.name,
 					date = new Date().toDateString(),
 					hours = new Date().getHours() + ':00';
-	
+
 				if (!extension.storage.data.analyzer) {
 					extension.storage.data.analyzer = {};
 				}
-	
+
 				if (!extension.storage.data.analyzer[date]) {
 					extension.storage.data.analyzer[date] = {};
 				}
-	
+
 				if (!extension.storage.data.analyzer[date][hours]) {
 					extension.storage.data.analyzer[date][hours] = {};
 				}
-	
+
 				if (!extension.storage.data.analyzer[date][hours][data]) {
 					extension.storage.data.analyzer[date][hours][data] = 0;
 				}
-	
+
 				extension.storage.data.analyzer[date][hours][data]++;
-	
+
 				chrome.storage.local.set({
 					analyzer: extension.storage.data.analyzer
 				});
@@ -182,7 +160,7 @@ document.addEventListener('it-message-from-web-accessible', function (message) {
 			if (!extension.storage.data.blocklist
 				|| typeof extension.storage.data.blocklist !== 'object') extension.storage.data.blocklist = {videos: {}, channels: {}};
 
-			switch(message.type) {
+			switch (message.type) {
 				case 'channel':
 					if (!extension.storage.data.blocklist.channels
 						|| typeof extension.storage.data.blocklist.channels !== 'object') extension.storage.data.blocklist.channels = {};
@@ -197,7 +175,7 @@ document.addEventListener('it-message-from-web-accessible', function (message) {
 						delete extension.storage.data.blocklist.channels[message.id];
 					}
 					break
-	
+
 				case 'video':
 					if (!extension.storage.data.blocklist.videos
 						|| typeof extension.storage.data.blocklist.videos !== 'object') extension.storage.data.blocklist.videos = {};
@@ -212,7 +190,7 @@ document.addEventListener('it-message-from-web-accessible', function (message) {
 					}
 					break
 			}
-	
+
 			chrome.storage.local.set({
 				blocklist: extension.storage.data.blocklist
 			});
@@ -221,15 +199,15 @@ document.addEventListener('it-message-from-web-accessible', function (message) {
 		case 'watched':
 			if (!extension.storage.data.watched
 				|| typeof extension.storage.data.watched !== 'object') extension.storage.data.watched = {};
-	
+
 			if (message.type === 'add') {
 				extension.storage.data.watched[message.id] = {
 					title: message.title
 				};
 			}
-	
+
 			if (message.type === 'remove') delete extension.storage.data.watched[message.id];
-	
+
 			chrome.storage.local.set({
 				watched: extension.storage.data.watched
 			});
