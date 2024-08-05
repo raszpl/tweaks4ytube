00ae3700433f35a9f3672cb1d666e1601ad39017
Rasz_pl <citizenr@gmail.com>
2024-08-05 06:26:08 +0200

    Update channel.js channelDefaultTab

--- js&css/web-accessible/www.youtube.com/channel.js
+++ js&css/web-accessible/www.youtube.com/channel.js
@@ -2,25 +2,26 @@
 4.6.0 CHANNEL
 ------------------------------------------------------------------------------*/
 /*--- DEFAULT CHANNEL TAB ----------------------------------------------------*/
-ImprovedTube.channelDefaultTab = function (a) {
+ImprovedTube.channelDefaultTab = function (a = document.querySelectorAll('A.ytd-video-owner-renderer, .ytd-channel-name>A')) {
 	const option = this.storage.channel_default_tab;
 
-	if (option
-		&& a
-		&& (a.classList.contains('ytd-video-owner-renderer') ||	a.parentNode?.classList.contains('ytd-channel-name'))
-		&& !a.href.endsWith(option)
-		&& this.regex.channel_home_page.test(a.href)) {
+	if (option) {
+		if (a instanceof Node
+			&& (a.classList.contains('ytd-video-owner-renderer') ||	a.parentNode?.classList.contains('ytd-channel-name'))
+			&& !a.href.endsWith(option)
+			&& this.regex.channel_home_page.test(a.href)) {
 
-		a.href = a.href.replace(this.regex.channel_home_page_postfix, '') + option;
-		a.onclick = function (event) {event.stopPropagation();};
+			if (document.documentElement.dataset.pageType === 'video' && a.parentNode.classList.contains('ytd-channel-name')) {
+				this.elements.yt_channel_name = a.parentNode.textContent;
+				this.elements.yt_channel_link = a.href;
+				this.howLongAgoTheVideoWasUploaded();
+				this.channelVideosCount();
+			}
 
-		if (a.parentNode.classList.contains('ytd-channel-name') && document.documentElement.dataset.pageType === 'video') {
-			this.elements.yt_channel_name = a.parentNode; // a.textContent ?
-			this.elements.yt_channel_link = a;
-			this.howLongAgoTheVideoWasUploaded();
-			this.channelVideosCount();
-		}
-	}
+			a.href = a.href.match(this.regex.channel)[0] + option;
+			a.onclick = function (event) {event.stopPropagation()};
+		} else if (a instanceof NodeList) { a.forEach(a => {a.href = a.href.match(this.regex.channel)[0] + option}) }
+	} else if (a instanceof NodeList) { a.forEach(a => {a.href = a.href.match(this.regex.channel)[0]}) }
 };
 /*--- PLAY ALL BUTTON --------------------------------------------------------*/
 ImprovedTube.channelPlayAllButton = function () {
