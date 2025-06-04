f2ddf78c3365c7cf1b2693bbec39131dfc408087
Rasz_pl <citizenr@gmail.com>
2025-06-04 17:19:45 +0200

    some light optimization -> shallow ratio

--- menu/skeleton-parts/themes.js
+++ menu/skeleton-parts/themes.js
@@ -1,7 +1,4 @@
-/*--------------------------------------------------------------
->>> THEMES
---------------------------------------------------------------*/
-
+/*--- THEMES -------------------------------------------------*/
 extension.skeleton.main.layers.section.themes = {
 	component: 'button',
 	variant: 'themes',
@@ -36,127 +33,99 @@ extension.skeleton.main.layers.section.themes = {
 		text: 'themes'
 	}
 };
-
-/*--------------------------------------------------------------
-# SECTION
---------------------------------------------------------------*/
-
+/*--- SECTION ------------------------------------------------*/
 extension.skeleton.main.layers.section.themes.on.click.section = {
 	component: 'section',
 	class: 'satus-section--transparent-card',
+	value: satus.storage.get('theme') == 'dark' ? 'dark' : 'light',
 
 	default: function () {
 		return {
-			component: 'label',
+			component: 'radio',
 			class: 'satus-label--' + (satus.storage.get('theme') == 'dark' ? 'dark-theme' : 'default-theme'),
 			text: satus.storage.get('theme') == 'dark' ? 'youtubesDark' : 'youtubesLight',
-			radio: {
-				component: 'radio',
-				storage: 'theme',
-				value: satus.storage.get('theme') == 'dark' ? 'dark' : 'light',
-				...(!satus.storage.get('theme') && { checked: true })
-			}
+			storage: 'theme',
+			value: satus.storage.get('theme') == 'dark' ? 'dark' : 'light',
+			...(!satus.storage.get('theme') && { checked: true })
 		}
 	},
 	opposite: function () {
 		return {
-			component: 'label',
+			component: 'radio',
 			class: 'satus-label--' + (satus.storage.get('theme') == 'dark' ? 'default-theme' : 'dark-theme'),
 			text: satus.storage.get('theme') == 'dark' ? 'youtubesLight' : 'youtubesDark',
-			radio: {
-				component: 'radio',
-				storage: 'theme',
-				value: satus.storage.get('theme') == 'dark' ? 'light' : 'dark',
-				...(satus.storage.get('theme') == 'dark' && { checked: true })
-			}
+			storage: 'theme',
+			value: satus.storage.get('theme') == 'dark' ? 'light' : 'dark',
+			...(satus.storage.get('theme') == 'dark' && { checked: true })
 		}
 	},
 	custom: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--custom-theme',
 		text: 'custom',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'custom',
-			on: {
-				click: {
-					section: {
-						component: 'section',
-						variant: 'card',
-						theme_primary_color: {
-							component: 'color-picker',
-							text: 'primaryColor',
-							value: [200, 200, 200]
-						},
-						theme_text_color: {
-							component: 'color-picker',
-							text: 'textColor',
-							value: [25, 25, 25]
-						}
+		storage: 'theme',
+		value: 'custom',
+		on: {
+			click: {
+				section: {
+					component: 'section',
+					variant: 'card',
+					title: 'Custom colors',
+
+					theme_primary_color: {
+						component: 'color-picker',
+						text: 'primaryColor',
+						value: [200, 200, 200]
+					},
+					theme_text_color: {
+						component: 'color-picker',
+						text: 'textColor',
+						value: [25, 25, 25]
 					}
 				}
 			}
 		}
 	},
 	black: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--black-theme',
 		text: 'black',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'black'
-		}
+		storage: 'theme',
+		value: 'black'
 	},
 	plain: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--plain-theme',
 		text: 'plain',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'plain'
-		}
+		storage: 'theme',
+		value: 'plain'
 	},
 	sunset: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--sunset-theme',
 		text: 'sunset',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'sunset'
-		}
+		storage: 'theme',
+		value: 'sunset'
 	},
 	night: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--night-theme',
 		text: 'night',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'night'
-		}
+		storage: 'theme',
+		value: 'night'
 	},
 	dawn: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--dawn-theme',
 		text: 'dawn',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'dawn'
-		}
+		storage: 'theme',
+		value: 'dawn'
 	},
 	desert: {
-		component: 'label',
+		component: 'radio',
 		class: 'satus-label--desert-theme',
 		text: 'desert',
-		radio: {
-			component: 'radio',
-			storage: 'theme',
-			value: 'desert'
-		}
+		storage: 'theme',
+		value: 'desert'
 	}
 };
