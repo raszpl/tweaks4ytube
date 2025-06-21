d771cea29d7a9d4c132c150dd0fc3de3332127dd
Rasz_pl <citizenr@gmail.com>
2025-06-21 10:29:54 +0200

    Update active-features.js non-clickable category headers, 'theme' showing correctly, reformat

--- menu/skeleton-parts/active-features.js
+++ menu/skeleton-parts/active-features.js
@@ -1,119 +1,15 @@
 /*--- ACTIVE FEATURES ----------------------------------------*/
 extension.skeleton.header.sectionEnd.menu.on.click.activeFeatures = {
 	component: 'button',
-	on: {
-		click: {
-			component: 'section',
-			variant: 'card',
-			on: {
-				render: function () {
-					let component = this;
+	category: true,
 
-					satus.search('', extension.skeleton, function (features) {
-						let skeleton = {};
-
-						for (const [key, feature] of Object.entries(features)) {
-							// search returns buttons now, skip those
-							if (feature.component === 'button') continue;
-
-							let default_value = feature.value,
-								value = feature.storage && satus.storage.get(feature.storage)
-									|| satus.storage.get(key),
-								parent_object = feature;
-
-							if (!satus.isset(default_value)) {
-								if (feature.component === 'select') {
-									if (feature.options && feature.options[0]) {
-										default_value = feature.options[0].value;
-									}
-								} else {
-									default_value = false;
-								}
-							}
-
-							if ((feature.component !== 'radio' && satus.isset(value) && value != default_value)
-								|| (feature.component === 'radio' && satus.isset(value) && value === default_value)) {
-								while (
-									parent_object.parentObject &&
-									!parent_object.parentObject.category
-								) {
-									parent_object = parent_object.parentObject;
-								}
-
-								if (parent_object.parentObject) {
-									let category = parent_object.parentObject.label.text,
-										subcategory,
-										text;
-
-									parent_object = feature;
-
-									while (
-										parent_object.parentObject &&
-										parent_object.parentObject.component !== 'button'
-									) {
-										parent_object = parent_object.parentObject;
-									}
-
-									parent_object = parent_object.parentObject;
-
-									if (parent_object) {
-										if (parent_object.label) {
-											subcategory = parent_object.label.text;
-										} else {
-											subcategory = parent_object.text;
-										}
-
-										if (category === subcategory) {
-											text = satus.locale.get(category);
-										} else {
-											text = satus.locale.get(category) + ' > ' + satus.locale.get(subcategory);
-										}
-
-										if (!skeleton[category + subcategory]) {
-											skeleton[category + subcategory] = {
-												component: 'section',
-												variant: 'card',
-												title: text
-											};
-										}
-
-										skeleton[category + subcategory][key] = feature;
-									} else {
-										if (!skeleton[category]) {
-											skeleton[category] = {
-												component: 'section',
-												variant: 'card',
-												title: category
-											};
-										}
-
-										skeleton[category][key] = feature;
-									}
-								}
-							}
-						}
-
-						if (Object.keys(skeleton).length === 0) {
-							satus.render({
-								component: 'span',
-								text: 'empty'
-							}, component);
-						} else {
-							satus.render(skeleton, component.parentNode);
-
-							component.remove();
-						}
-					});
-				}
-			}
-		}
-	},
-
+	on: {},
 	svg: {
 		component: 'svg',
 		attr: {
 			'viewBox': '0 0 24 24',
 			'fill': 'none',
+			'stroke': 'currentColor',
 			'stroke-width': '1.75'
 		},
 
@@ -131,7 +27,97 @@ extension.skeleton.header.sectionEnd.menu.on.click.activeFeatures = {
 		}
 	},
 	label: {
-		component: 'span',
-		text: 'activeFeatures'
+		component: 'span'
+	}
+};
+/*--- SECTION ------------------------------------------------*/
+extension.skeleton.header.sectionEnd.menu.on.click.activeFeatures.on.click = {
+	component: 'section',
+	variant: 'card',
+
+	on: {
+		render: function () {
+			const active = ['select', 'color-picker', 'radio', 'slider', 'shortcut', 'checkbox', 'switch'];
+			let component = this;
+
+			satus.search('', extension.skeleton, function (features) {
+				let skeleton = {};
+
+				for (const [key, feature] of Object.entries(features)) {
+					// only check potentially active elements
+					if (!active.includes(feature.component)) continue;
+
+					let parent = feature,
+						category,
+						subcategory,
+						text;
+
+					let default_value = feature.value,
+						value = feature.storage && satus.storage.get(feature.storage) || satus.storage.get(key),
+						parent_object = feature;
+
+					if (!satus.isset(default_value)) {
+						if (feature.component === 'select') {
+							if (feature.options && feature.options[0]) {
+								default_value = feature.options[0].value;
+							}
+						} else {
+							default_value = false;
+						}
+					}
+
+					// no setting, bailing out
+					if (!satus.isset(value)) continue;
+					// interested in values different from default, except for 'radio'
+					if (value !== default_value && (feature.component === 'radio')
+						|| (value === default_value && feature.component !== 'radio')) continue;
+
+					while (parent.parentObject && !parent.parentObject.category) {
+						parent = parent.parentObject;
+						subcategory = '';
+
+						if (parent.component == 'button') {
+							subcategory = satus.locale.get(parent.text);
+						} else if (parent.title) {
+							subcategory = satus.locale.get(parent.title);
+						}
+						text = !subcategory ? text : (!text ? subcategory : subcategory + ' > ' + text);
+						category = !category ? subcategory : (!subcategory ? category : subcategory + category);
+					}
+
+					if (parent.parentObject?.label?.text) {
+						subcategory = satus.locale.get(parent.parentObject.label.text);
+						text = subcategory + (!text ? '' : ' > ' + text);
+						category = (!category ? subcategory : subcategory + category).replace(/[^a-zA-Z]/g, '');
+					}
+
+					skeleton[category + '_label'] = {
+						component: 'section',
+						variant: 'label',
+						text: text
+					}
+
+					if (!skeleton[category]) {
+						skeleton[category] = {
+							component: 'section',
+							variant: 'card'
+						}
+					}
+
+					skeleton[category][key] = feature;
+				}
+
+				if (Object.keys(skeleton).length === 0) {
+					satus.render({
+						component: 'span',
+						text: 'empty'
+					}, component);
+				} else {
+					satus.render(skeleton, component.parentNode);
+
+					component.remove();
+				}
+			});
+		}
 	}
 };
