b52da84366d59ce9ec401ac9db5f9ef360ca1b8c
Rasz_pl <citizenr@gmail.com>
2025-06-21 10:14:30 +0200

    Update blocklist.js reformat, use default .text

--- menu/skeleton-parts/blocklist.js
+++ menu/skeleton-parts/blocklist.js
@@ -3,238 +3,8 @@ extension.skeleton.main.layers.section.blocklist = {
 	component: 'button',
 	variant: 'blocklist',
 	category: true,
-	on: {
-		click: {
-			blocklist_activate: {
-				component: 'switch',
-				class: 'satus-switch--activation',
-				text: 'activate'
-			},
-			section2: {
-				component: 'section',
-				variant: 'card',
 
-				channels: {
-					component: 'button',
-					text: 'channels',
-					style: {
-						justifyContent: 'space-between'
-					},
-					on: {
-						click: {
-							add_channel: {
-								component: 'section',
-								variant: 'card',
-								blocklist_add_channel: {
-									// FIXME finish this
-									component: 'button',
-									text: 'blocklist_add_channel',
-									var1: 'Channel URL',
-									var2: 'Optional thumbnail ULR',
-									var3: '',
-									on: {
-										click: {
-											component: 'popup',
-											variant: 'confirm',
-											content: function () {return 'unfinished functionality'}
-										}
-									}
-								}
-							},
-							channels: {
-								component: 'section',
-								variant: 'card',
-								on: {
-									render: function () {
-										const skeleton = {},
-											blocklist = satus.storage.data.blocklist;
-
-										if (blocklist && blocklist.channels) {
-											for (const key in blocklist.channels) {
-												const channel = blocklist.channels[key];
-
-												if (channel !== false) {
-													skeleton[key] = {
-														component: 'div',
-														variant: 'blocklist',
-														data: {
-															id: key
-														},
-
-														title: {
-															component: 'div',
-															text: channel.title || ''
-														},
-														delete: {
-															component: 'button',
-															on: {
-																click: function () {
-																	const blocklist = satus.storage.get('blocklist'),
-																		component = this.parentNode;
-
-																	if (blocklist && blocklist.channels) {
-																		delete blocklist.channels[component.dataset.id];
-
-																		satus.storage.set('blocklist', blocklist, function () {
-																			component.remove();
-																		});
-																	}
-																}
-															},
-
-															svg: {
-																component: 'svg',
-																attr: {
-																	'fill': 'currentColor',
-																	'viewBox': '0 0 24 24'
-																},
-
-																path: {
-																	component: 'path',
-																	attr: {
-																		'd': 'M6 19c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v10zM18 4h-2.5l-.7-.7A1 1 0 0 0 14 3H9.9a1 1 0 0 0-.7.3l-.7.7H6c-.6 0-1 .5-1 1s.5 1 1 1h12c.6 0 1-.5 1-1s-.5-1-1-1z'
-																	}
-																}
-															}
-														}
-													};
-												}
-											}
-										}
-
-										if (Object.keys(skeleton).length === 0) {
-											satus.render({
-												component: 'span',
-												text: 'empty'
-											}, this);
-										} else {
-											satus.render(skeleton, this);
-										}
-									}
-								}
-							}
-						}
-					},
-					list: {
-						component: 'span',
-						style: {
-							opacity: .64
-						},
-						on: {
-							render: function () {
-								const blocklist = satus.storage.get('blocklist');
-
-								if (blocklist && blocklist.channels && Object.keys(blocklist.channels).length) {
-									this.textContent = '('+Object.keys(blocklist.channels).length+')';
-								} else {
-									this.textContent = '(empty)';
-								}
-							}
-						}
-					}
-				},
-				videos: {
-					component: 'button',
-					text: 'videos',
-					style: {
-						justifyContent: 'space-between'
-					},
-					on: {
-						click: {
-							component: 'section',
-							variant: 'card',
-							on: {
-								render: function () {
-									const skeleton = {},
-										blocklist = satus.storage.get('blocklist');
-
-									if (blocklist && blocklist.videos) {
-										for (const key in blocklist.videos) {
-											const video = blocklist.videos[key];
-
-											if (video !== false) {
-												skeleton[key] = {
-													component: 'div',
-													variant: 'blocklist',
-													data: {
-														id: key
-													},
-
-													title: {
-														component: 'div',
-														text: video.title || ''
-													},
-													delete: {
-														component: 'button',
-														on: {
-															click: function () {
-																const blocklist = satus.storage.get('blocklist'),
-																	component = this.parentNode;
-
-																if (blocklist && blocklist.videos) {
-																	delete blocklist.videos[component.dataset.id];
-
-																	satus.storage.set('blocklist', blocklist, function () {
-																		component.remove();
-																	});
-																}
-															}
-														},
-
-														svg: {
-															component: 'svg',
-															attr: {
-																'fill': 'currentColor',
-																'viewBox': '0 0 24 24'
-															},
-
-															path: {
-																component: 'path',
-																attr: {
-																	'd': 'M6 19c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v10zM18 4h-2.5l-.7-.7A1 1 0 0 0 14 3H9.9a1 1 0 0 0-.7.3l-.7.7H6c-.6 0-1 .5-1 1s.5 1 1 1h12c.6 0 1-.5 1-1s-.5-1-1-1z'
-																}
-															}
-														}
-													}
-												};
-											}
-										}
-									}
-
-									if (Object.keys(skeleton).length === 0) {
-										satus.render({
-											component: 'span',
-											text: 'empty'
-										}, this);
-									} else {
-										satus.render(skeleton, this);
-									}
-								}
-							}
-						}
-					},
-					list: {
-						component: 'span',
-						style: {
-							opacity: .64
-						},
-						on: {
-							render: function () {
-								const blocklist = satus.storage.get('blocklist');
-
-								if (blocklist && blocklist.videos && Object.keys(blocklist.videos).length) {
-									this.textContent = '('+Object.keys(blocklist.videos).length+')';
-								} else {
-									this.textContent = '(empty)';
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	},
-
+	on: {},
 	icon: {
 		component: 'span',
 
@@ -265,7 +35,233 @@ extension.skeleton.main.layers.section.blocklist = {
 		}
 	},
 	label: {
-		component: 'span',
-		text: 'blocklist'
+		component: 'span'
+	}
+};
+/*--- SECTION ------------------------------------------------*/
+extension.skeleton.main.layers.section.blocklist.on.click = {
+	blocklist_activate: {
+		component: 'switch',
+		variant: 'activation',
+		text: 'activate'
+	},
+	section2: {
+		component: 'section',
+		variant: 'card',
+
+		channels: {
+			component: 'button',
+			style: {
+				justifyContent: 'space-between'
+			},
+			on: {
+				click: {
+					add_channel: {
+						component: 'section',
+						variant: 'card',
+						blocklist_add_channel: {
+							// FIXME finish this
+							component: 'button',
+							var1: 'Channel URL',
+							var2: 'Optional thumbnail ULR',
+							var3: '',
+							on: {
+								click: {
+									component: 'popup',
+									variant: 'confirm',
+									content: function () {return 'unfinished functionality'}
+								}
+							}
+						}
+					},
+					channels: {
+						component: 'section',
+						variant: 'card',
+						on: {
+							render: function () {
+								const skeleton = {},
+									blocklist = satus.storage.data.blocklist;
+
+								if (blocklist && blocklist.channels) {
+									for (const key in blocklist.channels) {
+										const channel = blocklist.channels[key];
+
+										if (channel !== false) {
+											skeleton[key] = {
+												component: 'div',
+												variant: 'blocklist',
+												data: {
+													id: key
+												},
+
+												title: {
+													component: 'div',
+													text: channel.title || ''
+												},
+												delete: {
+													component: 'button',
+													on: {
+														click: function () {
+															const blocklist = satus.storage.get('blocklist'),
+																component = this.parentNode;
+
+															if (blocklist && blocklist.channels) {
+																delete blocklist.channels[component.dataset.id];
+
+																satus.storage.set('blocklist', blocklist, function () {
+																	component.remove();
+																});
+															}
+														}
+													},
+
+													svg: {
+														component: 'svg',
+														attr: {
+															'fill': 'currentColor',
+															'viewBox': '0 0 24 24'
+														},
+
+														path: {
+															component: 'path',
+															attr: {
+																'd': 'M6 19c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v10zM18 4h-2.5l-.7-.7A1 1 0 0 0 14 3H9.9a1 1 0 0 0-.7.3l-.7.7H6c-.6 0-1 .5-1 1s.5 1 1 1h12c.6 0 1-.5 1-1s-.5-1-1-1z'
+															}
+														}
+													}
+												}
+											};
+										}
+									}
+								}
+
+								if (Object.keys(skeleton).length === 0) {
+									satus.render({
+										component: 'span',
+										text: 'empty'
+									}, this);
+								} else {
+									satus.render(skeleton, this);
+								}
+							}
+						}
+					}
+				}
+			},
+			list: {
+				component: 'span',
+				style: {
+					opacity: .64
+				},
+				on: {
+					render: function () {
+						const blocklist = satus.storage.get('blocklist');
+
+						if (blocklist && blocklist.channels && Object.keys(blocklist.channels).length) {
+							this.textContent = '('+Object.keys(blocklist.channels).length+')';
+						} else {
+							this.textContent = '(empty)';
+						}
+					}
+				}
+			}
+		},
+		videos: {
+			component: 'button',
+			style: {
+				justifyContent: 'space-between'
+			},
+			on: {
+				click: {
+					component: 'section',
+					variant: 'card',
+					on: {
+						render: function () {
+							const skeleton = {},
+								blocklist = satus.storage.get('blocklist');
+
+							if (blocklist && blocklist.videos) {
+								for (const key in blocklist.videos) {
+									const video = blocklist.videos[key];
+
+									if (video !== false) {
+										skeleton[key] = {
+											component: 'div',
+											variant: 'blocklist',
+											data: {
+												id: key
+											},
+
+											title: {
+												component: 'div',
+												text: video.title || ''
+											},
+											delete: {
+												component: 'button',
+												on: {
+													click: function () {
+														const blocklist = satus.storage.get('blocklist'),
+															component = this.parentNode;
+
+														if (blocklist && blocklist.videos) {
+															delete blocklist.videos[component.dataset.id];
+
+															satus.storage.set('blocklist', blocklist, function () {
+																component.remove();
+															});
+														}
+													}
+												},
+
+												svg: {
+													component: 'svg',
+													attr: {
+														'fill': 'currentColor',
+														'viewBox': '0 0 24 24'
+													},
+
+													path: {
+														component: 'path',
+														attr: {
+															'd': 'M6 19c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v10zM18 4h-2.5l-.7-.7A1 1 0 0 0 14 3H9.9a1 1 0 0 0-.7.3l-.7.7H6c-.6 0-1 .5-1 1s.5 1 1 1h12c.6 0 1-.5 1-1s-.5-1-1-1z'
+														}
+													}
+												}
+											}
+										};
+									}
+								}
+							}
+
+							if (Object.keys(skeleton).length === 0) {
+								satus.render({
+									component: 'span',
+									text: 'empty'
+								}, this);
+							} else {
+								satus.render(skeleton, this);
+							}
+						}
+					}
+				}
+			},
+			list: {
+				component: 'span',
+				style: {
+					opacity: .64
+				},
+				on: {
+					render: function () {
+						const blocklist = satus.storage.get('blocklist');
+
+						if (blocklist && blocklist.videos && Object.keys(blocklist.videos).length) {
+							this.textContent = '('+Object.keys(blocklist.videos).length+')';
+						} else {
+							this.textContent = '(empty)';
+						}
+					}
+				}
+			}
+		}
 	}
 };
